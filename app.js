d6dea1b75a906317d3f79102d8b415acd16a1b6b
Arslan <muhammadarslanrajput12@gmail.com>
2024-04-18 08:56:50 -0700

    javascript example

---
+++ app.js
@@ -0,0 +1,23 @@
+let random = Math.ceil(Math.random() * 7);
+console.log(random);
+document.write(random)
+
+
+// let oneimg= document.querySelectorAll("img");
+// console.log(oneimg)
+// oneimg[0].className="oneimg"
+
+
+
+
+// if(random===1){
+// let oneimg= document.querySelectorAll(".oneimg")
+// oneimg[0].innerHTML="aaa"
+// document.write("ddd")/
+// }
+
+
+let lable = document.querySelectorAll("#content");
+console.log(lable);
+// console.log(lable[0].innerHTML);
+// lable[0].innerHTML ="<i>Hello Mass</i>"
